91c0d5d2b4ecdcad92d7f0297017f0521cc2f96a
Sophia Post Ploposki <sophiapost.ploposki@icloud.com>
2024-09-29 15:10:46 -0300

    Add files via upload

---
+++ abóboras.py
@@ -0,0 +1,7 @@
+# entrada
+diâmetro = int(input("Digite o diâmetro: "))
+# processamento e saida
+if 15 <= diâmetro < 20:
+    print("A abóbora é média!")
+else:
+    print("Produto fora das medidas.")
